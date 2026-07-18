9cd95ef89ed6bde1a7c4d5eaa877ba90c7676936
Pranay <pranayvjadhao197@gmail.com>
2026-07-19 00:23:18 +0530

    Initial commit

---
+++ vowels.py
@@ -0,0 +1,12 @@
+ch = input("Enter any alphabet : ")
+
+if len(ch) == 1 and ch.isalpha():
+    ch = ch.upper()
+
+    match ch:
+        case 'A' | 'E' | 'I' | 'O' | 'U':
+            print("Vowel")
+        case _:
+            print("Consonant")
+else:
+    print("Error")
